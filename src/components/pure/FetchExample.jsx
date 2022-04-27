1c8269d9684de945d09745589757d186c8a8bfda
Tomas Padilla <padillatomasagustin@gmail.com>
2022-04-27 18:54:30 +0200

    Comenzamos video 21 - axios

--- src/components/pure/FetchExample.jsx
+++ src/components/pure/FetchExample.jsx
@@ -1,35 +1,53 @@
 import React, { useState, useEffect } from "react";
-import { getAllUsers } from "../../services/fetchService";
+import { getAllUsers, login } from "../../services/fetchService";
 
 const FetchExample = () => {
 	const [users, setUsers] = useState(null);
 	useEffect(() => {
-		obtainUsers();
+		// obtainUsers();
+		loginUser();
 	}, []);
 
-	const obtainUsers = () => {
-		getAllUsers()
+	// === GET USERS ===
+	// const obtainUsers = () => {
+	// 	getAllUsers()
+	// 		.then((response) => {
+	// 			console.log(response);
+	// 			setUsers(response);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log(err.message);
+	// 		})
+	// 		.finally(() => {
+	// 			console.log("Done Fetching");
+	// 			console.table(users);
+	// 		});
+	// };
+
+	// === LOGIN ===
+	const loginUser = () => {
+		login("eve.holt@reqres.in", "cityslicka")
 			.then((response) => {
 				console.log(response);
-				setUsers(response);
 			})
 			.catch((err) => {
 				console.log(err.message);
 			})
 			.finally(() => {
 				console.log("Done Fetching");
-				console.table(users);
 			});
 	};
-
 	return (
 		<div>
 			<h2>Fetch Example</h2>
+			<hr />
 			<h4>Users:</h4>
 			<h4>NO FUNCIONA LA API, pero vimos GET por Params, All</h4>
 			{/* {users.map((i, index) => {
 				<p key={index}>i.first_name</p>;
 			})} */}
+			<hr />
+			<h2>Login</h2>
 		</div>
 	);
 };
--- src/services/fetchService.js
+++ src/services/fetchService.js
@@ -1,5 +1,20 @@
 export const getAllUsers = async () => {
-	let response = await fetch("https://reqres.in/api/users");
+	let response = await fetch("https:reqres.in/api/users");
 	return response;
 	// console.log("GET ALL: ", response);
+};
+
+export const login = async (email, password) => {
+	let body = {
+		email,
+		password,
+	};
+	let response = await fetch("https://reqres.in/api/login", {
+		method: "POST",
+		cache: "no-cache",
+		headers: {
+			"Content-type": "application/json",
+		},
+		body,
+	});
 };
