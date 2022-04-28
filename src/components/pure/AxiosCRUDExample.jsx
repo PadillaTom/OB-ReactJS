e50794f2eb68d8cb7da7afd4b20793a03f338b59
Tomas Padilla <padillatomasagustin@gmail.com>
2022-04-28 07:26:57 +0200

    Axios , Formik, Yup

--- src/components/pure/AxiosCRUDExample.jsx
+++ src/components/pure/AxiosCRUDExample.jsx
@@ -1,20 +1,69 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { login } from "../../services/axiosCRUDService";
 
+const loginSchema = Yup.object().shape({
+	email: Yup.string().email("Debe ser un email Valido").required("Obligatorio"),
+	password: Yup.string().required("Obligatorio"),
+});
+
 const AxiosCRUDExample = () => {
-	const authUser = () => {
-		login("admin1@test.com", "12345678")
+	const initialCredentials = {
+		email: "",
+		password: "",
+	};
+
+	const authUser = ({ email, password }) => {
+		login(email, password)
 			.then((response) => {
-				console.log(response.data);
+				if (response.data) {
+					alert(JSON.stringify(response.data.token));
+					sessionStorage.setItem("token", response.data.token);
+				} else {
+					sessionStorage.removeItem("token");
+					throw new Error("Login Error");
+				}
 			})
 			.catch((err) => {
-				console.log(err);
+				alert("Error de Login");
+				sessionStorage.removeItem("token");
+			})
+			.finally(() => {
+				console.log("Login DONE");
 			});
 	};
 	return (
 		<div>
 			<h2>AxiosCRUDExample</h2>
-			<button onClick={authUser}>Login</button>
+			<Formik
+				initialValues={initialCredentials}
+				validationSchema={loginSchema}
+				onSubmit={async (values) => {
+					authUser(values);
+				}}
+			>
+				{/* FORM */}
+				{({ errors, touched }) => {
+					return (
+						<Form>
+							<label htmlFor="email">Email</label>
+							<Field id="email" name="email" placeholder="cosme@fulanito.com" />
+							{errors.email && touched.email && (
+								<ErrorMessage name="email" component="div"></ErrorMessage>
+							)}
+							<label htmlFor="password">Password</label>
+							<Field id="password" name="password" />
+							{errors.password && touched.password && (
+								<div className="error-msg">
+									<ErrorMessage name="password" component="div"></ErrorMessage>
+								</div>
+							)}
+							<button>Login</button>
+						</Form>
+					);
+				}}
+			</Formik>
 		</div>
 	);
 };
