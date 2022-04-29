52319fdfcea20a1375089f7a4cc8d919d1a2ff5a
Tomas Padilla <padillatomasagustin@gmail.com>
2022-04-29 10:41:57 +0200

    Redux OK

---
+++ src/components/containers/TodoFormContainer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { addTodo } from "../../store/actions/actions";
+import TodoForm from "../pure/TodoForm";
+
+const mapStateToProps = (state) => ({
+	// Not Needed
+});
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		submit: (text) => {
+			dispatch(addTodo(text));
+		},
+	};
+};
+
+const TodoFormContainer = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(TodoForm);
+
+export default TodoFormContainer;
